0bf58df7a67d8abae0fd7a249edbc5aff7c2866f
ER <raynaudo.ee@gmail.com>
2020-09-08 18:38:53 -0300

    Add trap Fix error color output

--- start.sh
+++ start.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 set -e
 
-GRN=$"\e[32;1m"
-OFF=$"\e[0m"
+GRN='\e[32;1m'
+RED='\033[0;31m'
+OFF='\e[0m'
+
+error() {
+  local message="$2"
+  local code="${3:-1}"
+  if [[ -n "$message" ]] ; then
+    echo -e "${RED} Error: ${message}; exiting with status ${code} ${OFF}"
+  else
+    echo -e "${RED} Error; exiting with status ${code} ${OFF}"
+  fi
+  kill 0
+  exit "${code}"
+}
+
+trap 'error ${LINENO}' ERR INT SIGINT
 
 lotus daemon 2>&1 &
 sleep 30
@@ -11,7 +26,7 @@ lotus log set-level ERROR
 
 while [ "$(lotus net peers | wc -l)" -eq 0 ]
 do
-  echo "${GRN}### Waiting for peers...${OFF}"
+  echo -e "${GRN}### Waiting for peers...${OFF}"
   sleep 5
 done
 
@@ -19,7 +34,5 @@ LOTUS_CHAIN_INDEX_CACHE=32768
 LOTUS_CHAIN_TIPSET_CACHE=8192
 LOTUS_RPC_TOKEN=$( cat /data/node/token )
 
-echo "${GRN}### Launching rosetta-filecoin-proxy${OFF}"
-rosetta-filecoin-proxy
-
-
+echo -e "${GRN}### Launching rosetta-filecoin-proxy${OFF}"
+rosetta-filecoin-proxy 2>&1
