e61ff61165aca98837b6242e973201884864af64
Juan Leni <juan.leni@zondax.ch>
2020-09-10 22:09:21 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -34,12 +34,6 @@ sleep 30
 
 lotus log set-level ERROR
 
-while [ "$(lotus net peers | wc -l)" -eq 0 ]
-do
-  echo -e "${GRN}### Waiting for peers...${OFF}"
-  sleep 5
-done
-
 LOTUS_CHAIN_INDEX_CACHE=32768
 LOTUS_CHAIN_TIPSET_CACHE=8192
 LOTUS_RPC_TOKEN=$( cat /data/node/token )
