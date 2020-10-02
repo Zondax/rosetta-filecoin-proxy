da7d37d004615181371be55569832d4ad2ae4f26
ER <raynaudo.ee@gmail.com>
2020-10-02 11:17:06 -0300

    Add time track func. Minor code reorder

--- rosetta/services/helpers.go
+++ rosetta/services/helpers.go
@@ -6,6 +6,7 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/zondax/rosetta-filecoin-proxy/rosetta/tools"
 	"reflect"
+	"time"
 
 	"github.com/coinbase/rosetta-sdk-go/types"
 	"github.com/filecoin-project/lotus/api"
@@ -15,6 +16,11 @@ import (
 )
 
 const unknownStr = "Unknown"
+
+func TimeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	Logger.Info(name, " took ", elapsed)
+}
 
 func BuildTipSetKeyHash(key filTypes.TipSetKey) (*string, error) {
 
@@ -58,11 +64,6 @@ func GetCurrencyData() *types.Currency {
 
 func GetMethodName(msg *filTypes.Message) (string, *types.Error) {
 
-	var (
-		actorCode cid.Cid
-		skipDB    bool
-	)
-
 	//Shortcut 1 - Method "0" corresponds to "MethodSend"
 	if msg.Method == 0 {
 		return "Send", nil
@@ -73,13 +74,22 @@ func GetMethodName(msg *filTypes.Message) (string, *types.Error) {
 		return "Constructor", nil
 	}
 
-	//Shortcut 3 - t1 and t3 address are always account actors
+	var (
+		actorCode cid.Cid
+		skipDB    bool
+	)
+
 	if len(msg.To.String()) > 2 {
+		//Shortcut 3 - addresses starting with "t1" or "t3" address are always account actors
 		addPrefix := msg.To.String()[0:2]
 		if addPrefix == "t1" || addPrefix == "t3" {
 			actorCode = builtin.AccountActorCodeID
 			skipDB = true
-		}
+			//TODO Shortcut 4 - addresses starting with "t0" or "t2" address are always miner actors
+		} /*else if addPrefix == "t0" || addPrefix == "t2" {
+			actorCode = builtin.StorageMinerActorCodeID
+			skipDB = true
+		} */
 	}
 
 	// Search for actor in cache
@@ -98,7 +108,7 @@ func GetMethodName(msg *filTypes.Message) (string, *types.Error) {
 	case builtin.CronActorCodeID:
 		method = builtin.MethodsCron
 	case builtin.AccountActorCodeID:
-		method = builtin.MethodsMultisig
+		method = builtin.MethodsAccount
 	case builtin.StoragePowerActorCodeID:
 		method = builtin.MethodsPower
 	case builtin.StorageMinerActorCodeID:
@@ -138,4 +148,13 @@ func GetSupportedOpList() []string {
 	}
 
 	return operations
+}
+
+func IsOpSupported(op string) bool {
+	supported, ok := SupportedOperations[op]
+	if ok && supported {
+		return true
+	}
+
+	return false
 }
