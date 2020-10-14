d21cead7f0ee25a58b562f9112657359e4342198
Juan Leni <lenijuan@gmail.com>
2020-10-14 20:29:33 +0200

    add additional CI test

---
+++ rosetta/tests/rosetta_cli_test2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o nounset -o pipefail -o errexit
+trap "exit 1" INT
+
+GRN=$'\e[32;1m'
+OFF=$'\e[0m'
+
+go get github.com/coinbase/rosetta-cli@v0.5.9
+
+rm -rf /tmp/rosetta-cli-test/*
+
+printf "${GRN}### Running rosetta-cli tests${OFF}\n"
+
+#Add all rosetta-cli checks here
+rosetta-cli check:data --configuration-file ./rosetta-config2.json
+
+printf "${GRN}### Tests finished.${OFF}\n"
