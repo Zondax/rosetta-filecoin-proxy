e4003252b764d5eb3bf64a4934e5611003bd06de
Eric Mokaya <emagembe@gmail.com>
2026-01-05 16:56:13 +0300

    Merge fix/comments into dev
    commit 304b688ebbb669deae894644a8452dba86e680b3
    Author: Eric Mokaya <emagembe@gmail.com>
    Date:   Mon Jan 5 15:27:18 2026 +0300
    
        chore: fix some function names in comment
    
    commit a5dde1bfb0cc51c9cec13b10e6f1fda471c6bbef
    Author: Prateek Rathod <emaillawrathod@gmail.com>
    Date:   Wed Dec 17 18:38:37 2025 +0530
    
        Feat/v2 f3 (#310)
    
        * feat: add v2 client support for lotus api
    
        * feat: add v2 usage on mempool endpoints
    
        * chore: .gitignore update
    
        * feat: add v2 usage to block endpoints
    
        * feat: allow blockheight and finality tag comparison on block endpoint
    
        * feat: add v2 and comp logic on /account endpoint and fix tests
    
        * tests: add v2 helper unit tests
    
        * feat: update /network endpoints and make testing easier
    
        * refac: minor fixes and refac with tipset compare logic on blocks and account
    
        * chore: update readme
    
        * chore: revert mempool changes and remove unit tests for now
    
        * refac: update v2 helper
    
        * feat: add support for chain anchor mode
    
        * revert readme.md
    
        * chore: revert network.go
    
        * refac: improve error handling and minor updates
    
        * feat: add f3 rosetta env info to /network endpoint
    
        * chore: fix makefile urls, add sync bool to network response
    
        * refac: minor fixes and remove silly comments
    
        * refac: revert error bc it's not required and is asserted anyway
    
        * refac: handler err
    
        * refac: cleaner switch for finality tag to fil type convert
    
        * refac: conditionally enable anchor mode if v2 enabled
    
        * refac: review comments
    
        * refac: improvements for network/list and utils added
    
        * chore: copy workflow file from master
    
        * chore: fix workflow
    
        * test: CI fix and add more tests for new feats
    
        * chore: revert secrets usage in workflow
    
        * chore: check ci without new tests
    
        * test: add unit test for v2_helpers
    
        * test: enable f3 integration test
    
        * ci: try fix
    
        * test: add finality tests
    
        ---------
    
        Co-authored-by: Eric Mokaya <emagembe@gmail.com>
    
    commit e19ce75264a570392b351047ee980edfbebee812
    Merge: b49c4f0 c5523d1
    Author: Prateek Rathod <emaillawrathod@gmail.com>
    Date:   Mon Oct 13 11:49:27 2025 +0200
    
        Merge branch 'master' into dev
    
    commit b49c4f04b12e87028f01e1aca073fcaff02b53da
    Merge: a5411b3 4c33910
    Author: Ezequiel Raynaudo <raynaudo.ee@gmail.com>
    Date:   Fri Sep 29 08:42:57 2023 -0300
    
        Merge branch 'master' into dev
    
    commit a5411b3d4f2ceb25b71d493ba6f99df3d65664d3
    Author: Ezequiel Raynaudo <raynaudo.ee@gmail.com>
    Date:   Fri Sep 29 08:42:02 2023 -0300
    
        Merge branch 'master' into dev
    
    commit 7f86dee9b56aa45e9cc8abe5113d35504baa02c4
    Author: Ezequiel Raynaudo <raynaudo.ee@gmail.com>
    Date:   Tue Jun 7 10:38:59 2022 -0300
    
        Merge branch 'master' into dev
    
    commit 609ad18be5b35b8d1e90cef74ed4fc8711f24d36
    Merge: 7f34650 bb7f619
    Author: Juan Leni <juan.leni@zondax.ch>
    Date:   Tue Jul 27 16:57:54 2021 +0200
    
        Merge pull request #145 from Zondax/fix/reject-1.5-lotus
    
        dont connect to lotus node > 1.5.0
    
    commit bb7f619f2c2f40a73c9a3d197d46dd6fff26707d
    Author: rllola <rllola80@gmail.com>
    Date:   Tue Jul 27 14:44:04 2021 +0200
    
        dont connect to lotus node > 1.5.0

--- rosetta/services/mempool.go
+++ rosetta/services/mempool.go
@@ -12,14 +12,14 @@ import (
 	"github.com/filecoin-project/lotus/api"
 )
 
-// BlockAPIService implements the server.BlockAPIServicer interface.
+// MemPoolAPIService implements the server.BlockAPIServicer interface.
 type MemPoolAPIService struct {
 	network    *types.NetworkIdentifier
 	node       api.FullNode
 	rosettaLib *filLib.RosettaConstructionFilecoin
 }
 
-// NewBlockAPIService creates a new instance of a BlockAPIService.
+// NewMemPoolAPIService creates a new instance of a BlockAPIService.
 func NewMemPoolAPIService(network *types.NetworkIdentifier, api *api.FullNode, r *filLib.RosettaConstructionFilecoin) server.MempoolAPIServicer {
 	return &MemPoolAPIService{
 		network:    network,
