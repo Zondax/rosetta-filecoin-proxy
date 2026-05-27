Zondax/rosetta-filecoin-proxy

commit a5dde1bfb0cc51c9cec13b10e6f1fda471c6bbef

Feat/v2 f3 (#310)
* feat: add v2 client support for lotus api

* feat: add v2 usage on mempool endpoints

* chore: .gitignore update

* feat: add v2 usage to block endpoints

* feat: allow blockheight and finality tag comparison on block endpoint

* feat: add v2 and comp logic on /account endpoint and fix tests

* tests: add v2 helper unit tests

* feat: update /network endpoints and make testing easier

* refac: minor fixes and refac with tipset compare logic on blocks and account

* chore: update readme

* chore: revert mempool changes and remove unit tests for now

* refac: update v2 helper

* feat: add support for chain anchor mode

* revert readme.md

* chore: revert network.go

* refac: improve error handling and minor updates

* feat: add f3 rosetta env info to /network endpoint

* chore: fix makefile urls, add sync bool to network response

* refac: minor fixes and remove silly comments

* refac: revert error bc it's not required and is asserted anyway

* refac: handler err

* refac: cleaner switch for finality tag to fil type convert

* refac: conditionally enable anchor mode if v2 enabled

* refac: review comments

* refac: improvements for network/list and utils added

* chore: copy workflow file from master

* chore: fix workflow

* test: CI fix and add more tests for new feats

* chore: revert secrets usage in workflow

* chore: check ci without new tests

* test: add unit test for v2_helpers

* test: enable f3 integration test

* ci: try fix

* test: add finality tests

---------

Co-authored-by: Eric Mokaya <emagembe@gmail.com>

rosetta/services/account.go
@@ -2,30 +2,33 @@ package services
 
 import (
 	"context"
+	"strconv"
+
 	rosettaFilecoinLib "github.com/zondax/rosetta-filecoin-lib"
 	"github.com/zondax/rosetta-filecoin-lib/actors"
-	"strconv"
 
 	"github.com/coinbase/rosetta-sdk-go/server"
 	"github.com/coinbase/rosetta-sdk-go/types"
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api/v2api"
 	filTypes "github.com/filecoin-project/lotus/chain/types"
 )
 
 // AccountAPIService implements the server.BlockAPIServicer interface.
 type AccountAPIService struct {
 	network    *types.NetworkIdentifier
-	node       api.FullNode
+	v1Node     api.FullNode
+	v2Node     v2api.FullNode
 	rosettaLib *rosettaFilecoinLib.RosettaConstructionFilecoin
 }
 
-// NewBlockAPIService creates a new instance of a BlockAPIService.
-func NewAccountAPIService(network *types.NetworkIdentifier, node *api.FullNode, r *rosettaFilecoinLib.RosettaConstructionFilecoin) server.AccountAPIServicer {
+func NewAccountAPIService(network *types.NetworkIdentifier, v1API *api.FullNode, v2API *v2api.FullNode, r *rosettaFilecoinLib.RosettaConstructionFilecoin) server.AccountAPIServicer {
 	return &AccountAPIService{
 		network:    network,
-		node:       *node,
+		v1Node:     *v1API,
+		v2Node:     *v2API,
 		rosettaLib: r,
 	}
 }
@@ -34,7 +37,7 @@ func NewAccountAPIService(network *types.NetworkIdentifier, node *api.FullNode, 
 func (a AccountAPIService) AccountBalance(ctx context.Context,
 	request *types.AccountBalanceRequest) (*types.AccountBalanceResponse, *types.Error) {
 
-	errNet := ValidateNetworkId(ctx, &a.node, request.NetworkIdentifier)
+	errNet := ValidateNetworkId(ctx, &a.v1Node, request.NetworkIdentifier)
 	if errNet != nil {
 		return nil, errNet
 	}
@@ -44,88 +47,53 @@ func (a AccountAPIService) AccountBalance(ctx context.Context,
 		return nil, BuildError(ErrInvalidAccountAddress, nil, true)
 	}
 
-	// Check sync status
-	status, syncErr := CheckSyncStatus(ctx, &a.node)
+	status, syncErr := CheckSyncStatus(ctx, &a.v1Node)
 	if syncErr != nil {
 		return nil, syncErr
 	}
+
 	if !status.IsSynced() {
 		return nil, BuildError(ErrNodeNotSynced, nil, true)
 	}
 
-	useHeadTipSet := false
+	var requestedHeight int64 = -1
+	if request.BlockIdentifier != nil && request.BlockIdentifier.Index != nil {
+		requestedHeight = *request.BlockIdentifier.Index
+	}
 
-	var queryTipSet *filTypes.TipSet    // TipSet to use on StateGetActor
-	var responseTipSet *filTypes.TipSet // TipSet to get queryTipSetHeight and queryTipSetHash values for response
-	var headTipSet *filTypes.TipSet     // Chain's head TipSet
+	finalityTag, err := GetFinalityTagFromNetworkIdentifier(request.NetworkIdentifier)
+	if err != nil {
+		return nil, BuildError(ErrUnableToGetLatestBlk, err, true)
+	}
 
-	var fixedQueryHeight int64
-	var originalQueryHeight int64
+	resolution, err := ResolveTipSetForFinality(ctx, a.v1Node, a.v2Node, requestedHeight, finalityTag)
+	if err != nil {
+		return nil, BuildError(ErrUnableToGetTipset, err, true)
+	}
 
-	// To return in response
-	var queryTipSetHeight int64
-	var queryTipSetHash *string
+	tipSet := resolution.TipSet
+	requestedHeight = resolution.Height
 
-	headTipSet, filErr = a.node.ChainHead(ctx)
+	var queryTipSet *filTypes.TipSet
+	var responseTipSet *filTypes.TipSet
+
+	// Now we need to get the appropriate query tipset for StateGetActor
+	// StateGetActor computes the state at parent's tipSet, so we need to query at (height + 1)
+	queryTipSet, filErr = a.v1Node.ChainGetTipSetByHeight(ctx, abi.ChainEpoch(requestedHeight+1), filTypes.EmptyTSK)
 	if filErr != nil {
-		return nil, BuildError(ErrUnableToGetLatestBlk, filErr, true)
+		// If we can't get the +1 tipset, use the current tipset
+		queryTipSet = tipSet
 	}
-
-	if request.BlockIdentifier != nil {
-		if request.BlockIdentifier.Index == nil {
-			return nil, BuildError(ErrInsufficientQueryInputs, nil, true)
-		}
-
-		originalQueryHeight = *request.BlockIdentifier.Index
-		// From lotus v1.5 and on, StateGetActor computes the state at parent's tipSet.
-		// To get the state on the requested height, we need to query the block at (height + 1).
-
-		// First, check if we're querying the head tipSet, if not, query the +1 tipSet
-		if originalQueryHeight == int64(headTipSet.Height()) {
-			useHeadTipSet = true
-		} else {
-			fixedQueryHeight = originalQueryHeight + 1
-		}
-	} else {
-		// If BlockIdentifier is not set, query chain's head tipSet
-		useHeadTipSet = true
-	}
-
-	if useHeadTipSet {
-		queryTipSet = headTipSet
-		responseTipSet, filErr = a.node.ChainGetTipSet(ctx, headTipSet.Parents())
-		if filErr != nil {
-			return nil, BuildError(ErrUnableToGetParentBlk, filErr, true)
-		}
-	} else {
-		queryTipSet, filErr = a.node.ChainGetTipSetByHeight(ctx, abi.ChainEpoch(fixedQueryHeight), filTypes.EmptyTSK)
-		if filErr != nil {
-			return nil, BuildError(ErrUnableToGetBlk, filErr, true)
-		}
-		if queryTipSet.Height() == abi.ChainEpoch(originalQueryHeight) {
-			// Means that the tipset at originalQueryHeight + 1 has no blocks, so we need to skip it
-			fixedQueryHeight = originalQueryHeight + 2
-
-			// Repeat the call with the updated height
-			queryTipSet, filErr = a.node.ChainGetTipSetByHeight(ctx, abi.ChainEpoch(fixedQueryHeight), filTypes.EmptyTSK)
-			if filErr != nil {
-				return nil, BuildError(ErrUnableToGetBlk, filErr, true)
-			}
-		}
-		responseTipSet, filErr = a.node.ChainGetTipSetByHeight(ctx, abi.ChainEpoch(originalQueryHeight), filTypes.EmptyTSK)
-		if filErr != nil {
-			return nil, BuildError(ErrUnableToGetBlk, filErr, true)
-		}
-	}
+	responseTipSet = tipSet
 
 	var balanceStr = "0"
-	queryTipSetHeight = int64(responseTipSet.Height())
-	queryTipSetHash, filErr = BuildTipSetKeyHash(responseTipSet.Key())
+	queryTipSetHeight := int64(responseTipSet.Height())
+	queryTipSetHash, filErr := BuildTipSetKeyHash(responseTipSet.Key())
 	if filErr != nil {
 		return nil, BuildError(ErrUnableToBuildTipSetHash, filErr, true)
 	}
 
-	actor, err := a.node.StateGetActor(ctx, addr, queryTipSet.Key())
+	actor, err := a.v1Node.StateGetActor(ctx, addr, queryTipSet.Key())
 	if err != nil {
 		// If actor is not found on chain, return 0 balance
 		return &types.AccountBalanceResponse{
@@ -153,20 +121,20 @@ func (a AccountAPIService) AccountBalance(ctx context.Context,
 		switch request.AccountIdentifier.SubAccount.Address {
 		case LockedBalanceStr:
 			lockedBalance := actor.Balance
-			spendableBalance, err := a.node.MsigGetAvailableBalance(ctx, addr, queryTipSet.Key())
+			spendableBalance, err := a.v1Node.MsigGetAvailableBalance(ctx, addr, queryTipSet.Key())
 			if err != nil {
 				return nil, BuildError(ErrUnableToGetBalance, err, true)
 			}
 			lockedBalance.Sub(lockedBalance.Int, spendableBalance.Int)
 			balanceStr = lockedBalance.String()
 		case SpendableBalanceStr:
-			spendableBalance, err := a.node.MsigGetAvailableBalance(ctx, addr, queryTipSet.Key())
+			spendableBalance, err := a.v1Node.MsigGetAvailableBalance(ctx, addr, queryTipSet.Key())
 			if err != nil {
 				return nil, BuildError(ErrUnableToGetBalance, err, true)
 			}
 			balanceStr = spendableBalance.String()
 		case VestingScheduleStr:
-			vestingSch, err := a.node.MsigGetVestingSchedule(ctx, addr, queryTipSet.Key())
+			vestingSch, err := a.v1Node.MsigGetVestingSchedule(ctx, addr, queryTipSet.Key())
 			if err != nil {
 				return nil, BuildError(ErrUnableToGetVesting, err, true)
 			}

rosetta/services/account_test.go
@@ -2,18 +2,20 @@ package services
 
 import (
 	"context"
+	"reflect"
+	"testing"
+
 	"github.com/filecoin-project/go-state-types/abi"
 	filTypes "github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/mock"
 	rosettaFilecoinLib "github.com/zondax/rosetta-filecoin-lib"
-	"reflect"
-	"testing"
 
 	"github.com/coinbase/rosetta-sdk-go/server"
 	"github.com/coinbase/rosetta-sdk-go/types"
 	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api/v2api"
 	mocks "github.com/zondax/rosetta-filecoin-proxy/rosetta/services/mocks"
 )
 
@@ -29,7 +31,8 @@ func TestAccountAPIService_AccountBalance(t *testing.T) {
 
 	type fields struct {
 		network *types.NetworkIdentifier
-		node    api.FullNode
+		v1Node  api.FullNode
+		v2Node  v2api.FullNode
 	}
 	type args struct {
 		ctx     context.Context
@@ -106,7 +109,8 @@ func TestAccountAPIService_AccountBalance(t *testing.T) {
 			name: "AvailableBalanceOfMultiSig",
 			fields: fields{
 				network: NetworkID,
-				node:    &nodeMock,
+				v1Node:  &nodeMock,
+				v2Node:  nil,
 			},
 			args: args{
 				ctx: context.Background(),
@@ -141,7 +145,8 @@ func TestAccountAPIService_AccountBalance(t *testing.T) {
 			name: "LockedBalanceOfMultiSig",
 			fields: fields{
 				network: NetworkID,
-				node:    &nodeMock,
+				v1Node:  &nodeMock,
+				v2Node:  nil,
 			},
 			args: args{
 				ctx: context.Background(),
@@ -180,7 +185,8 @@ func TestAccountAPIService_AccountBalance(t *testing.T) {
 			name: "VestingSchedule",
 			fields: fields{
 				network: NetworkID,
-				node:    &nodeMock,
+				v1Node:  &nodeMock,
+				v2Node:  nil,
 			},
 			args: args{
 				ctx: context.Background(),
@@ -219,7 +225,8 @@ func TestAccountAPIService_AccountBalance(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			a := AccountAPIService{
 				network: tt.fields.network,
-				node:    tt.fields.node,
+				v1Node:  tt.fields.v1Node,
+				v2Node:  tt.fields.v2Node,
 			}
 			got, got1 := a.AccountBalance(tt.args.ctx, tt.args.request)
 			if !reflect.DeepEqual(got, tt.want) {
@@ -235,7 +242,8 @@ func TestAccountAPIService_AccountBalance(t *testing.T) {
 func TestNewAccountAPIService(t *testing.T) {
 	type args struct {
 		network *types.NetworkIdentifier
-		node    *api.FullNode
+		v1Node  *api.FullNode
+		v2Node  *v2api.FullNode
 	}
 	tests := []struct {
 		name string
@@ -246,7 +254,7 @@ func TestNewAccountAPIService(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewAccountAPIService(tt.args.network, tt.args.node, rosettaLib); !reflect.DeepEqual(got, tt.want) {
+			if got := NewAccountAPIService(tt.args.network, tt.args.v1Node, tt.args.v2Node, rosettaLib); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewAccountAPIService() = %v, want %v", got, tt.want)
 			}
 		})

.github/workflows/main.yml
@@ -4,9 +4,9 @@ name: rosetta-cli
 # events but only for the master branch
 on:
   push:
-    branches: [ master ]
+    branches: [master, dev]
   pull_request:
-    branches: [ master ]
+    branches: [master, dev]
 
 jobs:
   build:
@@ -39,18 +39,18 @@ jobs:
   rosetta_cli:
     name: Rosetta CLI integration tests
     runs-on: zondax-runners
-    needs: [ build ]
+    needs: [build]
     strategy:
       fail-fast: false
       matrix:
         include:
-#          - network: mainnet
-#            url: LOTUS_MAINNET_RPC_URL
-#          - network: devnet
-#            url: LOTUS_DEVNET_RPC_URL
+          # - network: mainnet
+          #   url: LOTUS_MAINNET_RPC_URL
+          #          - network: devnet
+          #            url: LOTUS_DEVNET_RPC_URL
           - network: calibration
             url: LOTUS_CALIBRATION_RPC_URL
-            token: LOTUS_CALIBRATION_RPC_TOKEN
+          #   token: LOTUS_CALIBRATION_RPC_TOKEN
     timeout-minutes: 120
     steps:
       - run: sudo apt update && sudo apt install -yy build-essential bzr git jq pkg-config mesa-opencl-icd ocl-icd-opencl-dev hwloc libhwloc-dev curl
@@ -59,7 +59,7 @@ jobs:
           submodules: true
       - uses: actions/setup-go@v5
         with:
-          go-version: '1.23.10' # The Go version to download (if necessary) and use.
+          go-version: "1.23.10" # The Go version to download (if necessary) and use.
       - uses: actions-rs/toolchain@v1
         with:
           profile: minimal
@@ -75,6 +75,6 @@ jobs:
         run: ./rosetta-filecoin-proxy&
         env:
           LOTUS_RPC_URL: ${{ secrets[matrix.url] }}
-          LOTUS_RPC_TOKEN: ${{ secrets[matrix.token] }}
+          # LOTUS_RPC_TOKEN: ${{ secrets[matrix.token] }}
       - run: ./rosetta_cli_test_PR.sh ${{ matrix.network }}
         working-directory: ./rosetta/tests

.gitignore
@@ -4,3 +4,4 @@
 rosetta-filecoin-proxy
 .DS_Store
 extern/.DS_Store
+main

Makefile
@@ -55,8 +55,11 @@ gitclean:
 	git clean -xfd
 	git submodule foreach --recursive git clean -xfd
 
+run: build
+	LOTUS_RPC_URL=https://node-fil-calibration-light.zondax.ch/rpc ENABLE_FINALITY_ANCHOR=1 ./rosetta-filecoin-proxy
+
 test_calibration_macos: build
-	LOTUS_RPC_URL=https://node-fil-calibration-light.zondax.dev/rpc/v1  ./rosetta-filecoin-proxy &
+	LOTUS_RPC_URL=https://node-fil-calibration-light.zondax.ch/rpc  ./rosetta-filecoin-proxy &
 
 generate_mocks:
 	@if [ -z "$(LOTUS_DIR)" ]; then \

commit be490411576689695be7777758b51682bbfadbb5

fix(account): walk +1 tipset lookup past null epochs and chain head
Resolves the regression introduced by PR #310 ("Feat/v2 f3") that
surfaced post-NV28 on mainnet: AccountBalance returned stale balances
for recent heights whenever the requested height's +1 epoch was null.

Background

  StateGetActor returns state at the PARENT of the tipset key it's
  given, so to read state at the end of `resolvedHeight` we need a
  tipset whose parent is at that height — naturally a tipset at
  `resolvedHeight + 1`. The previous code did a single unconditional
  ChainGetTipSetByHeight(resolvedHeight+1) and used its key as-is.

  Two failure modes that the pre-PR #310 code handled but the
  post-PR #310 ad-hoc lookup did not:

  1. `resolvedHeight + 1` is null. Lotus's ChainGetTipSetByHeight
     falls back to the latest non-null tipset whose height is <=
     the requested epoch, which can be tipset-at-`resolvedHeight`
     (or even lower). Using that fallback's Key() made
     StateGetActor read state at the parent of `resolvedHeight` —
     i.e., state at end of `resolvedHeight - 1`, silently dropping
     every message that landed at `resolvedHeight`.

  2. `resolvedHeight` is already at chain head. No successor exists
     yet. The previous code claimed a balance "at head" but the
     state read was actually at end of head-1, mislabeling the
     response.

Fix

  Walk forward from `resolvedHeight + 1` until a tipset strictly
  above `resolvedHeight` is found (detecting Lotus's null-tipset
  fallback by the returned height equaling the requested one).
  Bound the walk by ChainHead — if we'd walk past head, fall
  through to the at-head branch which reads state at parent of
  resolvedHeight and shifts the response identifier to head-1
  to keep (balance, block_identifier) self-consistent.

  Safety bound of 50 consecutive null epochs guards against any
  pathological-mock or non-Lotus implementation that never
  produces a non-null tipset; the at-head fallback fires.

rosetta/services/account.go
@@ -72,19 +72,74 @@ func (a AccountAPIService) AccountBalance(ctx context.Context,
 	}
 
 	tipSet := resolution.TipSet
-	requestedHeight = resolution.Height
+	resolvedHeight := resolution.Height
 
 	var queryTipSet *filTypes.TipSet
 	var responseTipSet *filTypes.TipSet
 
-	// Now we need to get the appropriate query tipset for StateGetActor
-	// StateGetActor computes the state at parent's tipSet, so we need to query at (height + 1)
-	queryTipSet, filErr = a.v1Node.ChainGetTipSetByHeight(ctx, abi.ChainEpoch(requestedHeight+1), filTypes.EmptyTSK)
-	if filErr != nil {
-		// If we can't get the +1 tipset, use the current tipset
-		queryTipSet = tipSet
+	// StateGetActor returns state at the PARENT of the tipset key it's
+	// given, so to read state at the end of `resolvedHeight` we need a
+	// tipset whose parent is at `resolvedHeight` — naturally a tipset
+	// at `resolvedHeight + 1`. Two failure modes the pre-PR #310 code
+	// handled but the post-PR #310 ad-hoc lookup did not:
+	//
+	//   1. `resolvedHeight + 1` is null. Lotus's ChainGetTipSetByHeight
+	//      falls back to the latest non-null tipset whose height is
+	//      <= the requested epoch, which can be tipset-at-`resolvedHeight`
+	//      (or even lower). Using that fallback's Key() makes
+	//      StateGetActor read state at the parent of `resolvedHeight` —
+	//      i.e., state at the end of `resolvedHeight - 1`, silently
+	//      dropping every message that landed at `resolvedHeight`.
+	//      Walk forward until we find a tipset strictly above
+	//      `resolvedHeight`.
+	//
+	//   2. `resolvedHeight` is already at chain head. No successor
+	//      exists yet. We can't observe state at the end of head;
+	//      best behavior is to report state at the end of head-1 AND
+	//      shift the response's block_identifier down to head-1 so
+	//      (balance, block_identifier) remain self-consistent.
+	queryTipSet = nil
+	headForLoop, headErr := a.v1Node.ChainHead(ctx)
+	if headErr != nil {
+		return nil, BuildError(ErrUnableToGetLatestBlk, headErr, true)
 	}
-	responseTipSet = tipSet
+	headHeight := int64(headForLoop.Height())
+	const maxNullStretch = 50 // safety bound; consecutive null epochs are rare
+	for offset := int64(1); offset <= maxNullStretch; offset++ {
+		target := resolvedHeight + offset
+		if target > headHeight {
+			// Walked past head — by definition, no successor tipset
+			// exists yet. Fall through to the at-head branch below.
+			break
+		}
+		candidate, candidateErr := a.v1Node.ChainGetTipSetByHeight(ctx, abi.ChainEpoch(target), filTypes.EmptyTSK)
+		if candidateErr != nil {
+			break
+		}
+		if int64(candidate.Height()) > resolvedHeight {
+			queryTipSet = candidate
+			break
+		}
+		// Otherwise Lotus bumped us backward — (resolvedHeight + offset)
+		// is null; try the next offset.
+	}
+	if queryTipSet == nil {
+		// No successor available (either at head or no non-null tipset
+		// within the safety bound). Use the resolution tipset and
+		// self-consistently label the response with its parent so
+		// (balance, identifier) remain aligned: balance reflects state
+		// at parent(resolvedHeight) = end of resolvedHeight - 1, and
+		// the identifier reports the height whose state is actually
+		// returned.
+		parentTipSet, parentErr := a.v1Node.ChainGetTipSet(ctx, tipSet.Parents())
+		if parentErr != nil {
+			return nil, BuildError(ErrUnableToGetParentBlk, parentErr, true)
+		}
+		queryTipSet = tipSet
+		responseTipSet = parentTipSet
+	} else {
+		responseTipSet = tipSet
+	}
 
 	var balanceStr = "0"
 	queryTipSetHeight := int64(responseTipSet.Height())

rosetta/services/account_balance_tipset_test.go
@@ -128,9 +128,6 @@ func TestAccountBalance_HistoricalHeight_HappyPath(t *testing.T) {
 	assert.Equal(t, *tipsetAt100Hash, got.BlockIdentifier.Hash)
 }
 
-// Silence unused-import warning for abi when no other test in this file
-// uses it yet; subsequent commits add tests that do.
-var _ abi.ChainEpoch
 
 // TestAccountBalance_NullTipsetAtRequestedPlusOne_Regression pins down a
 // concrete regression introduced by PR #310 ("Feat/v2 f3") that

rosetta/services/account_test.go
@@ -91,6 +91,14 @@ func TestAccountAPIService_AccountBalance(t *testing.T) {
 			nil)
 	nodeMock.On("ChainGetTipSetByHeight", mock.Anything, mock.Anything, mock.Anything).
 		Return(mockTipSet, nil)
+	// The +1-tipset fix falls back to ChainGetTipSet(tipSet.Parents())
+	// when no successor exists (the at-head case). The broad-brush
+	// mock above returns mockTipSet for every epoch — which simulates
+	// a chain where every epoch past the requested one is null — so
+	// the fix's at-head branch fires. Register the parent lookup so
+	// the mock doesn't error on an unexpected call.
+	nodeMock.On("ChainGetTipSet", mock.Anything, mock.Anything).
+		Return(mockTipSet, nil)
 	nodeMock.On("StateGetActor", mock.Anything, mock.Anything, mock.Anything).
 		Return(mockMsigActor, nil)
 	nodeMock.On("MsigGetAvailableBalance", mock.Anything, mock.Anything, mock.Anything).